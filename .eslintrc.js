987d9226420ab44f5a6848237910ba82ee24c3f7
Joonas Nuutinen <joonas.nuutinen@autua.fi>
2020-12-18 21:16:29 +0200

    Disable warnings on unused vars

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,6 +46,9 @@ module.exports = {
       "error", { "before": true, "after": true }
     ],
     "no-console": "error",
-    "react/prop-types": 0
+    "react/prop-types": 0,
+    "no-unused-vars": [
+      "error", { "argsIgnorePattern": "^_" }
+  ],
   }
 }
